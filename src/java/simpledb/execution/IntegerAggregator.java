cef91e909d930a866c52c6d72d03c570745bc8ae
盛宇 <fangshengyu.fsy@antgroup.com>
2022-06-30 15:59:52 +0800

    implement IntegerAggregator

--- src/java/simpledb/execution/IntegerAggregator.java
+++ src/java/simpledb/execution/IntegerAggregator.java
@@ -8,7 +8,10 @@ import simpledb.storage.Tuple;
 import simpledb.storage.TupleDesc;
 import simpledb.transaction.TransactionAbortedException;
 
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.NoSuchElementException;
 
 /**
@@ -26,13 +29,14 @@ public class IntegerAggregator implements Aggregator {
 
     private Op operator;
 
-    private Tuple[] tuples;
+    private Map<Field, Tuple> aggregateFieldToTuple;
 
-    private int cursor;
+    //Used when calculate AVG
+    private Map<Field, int[]> aggregateFieldToFieldCountAndSum;
 
     /**
      * Aggregate constructor
-     * 
+     *
      * @param gbfield
      *            the 0-based index of the group-by field in the tuple, or
      *            NO_GROUPING if there is no grouping
@@ -51,39 +55,70 @@ public class IntegerAggregator implements Aggregator {
         this.gbfieldtype = gbfieldtype;
         this.afield = afield;
         this.operator = what;
+        this.aggregateFieldToTuple = new LinkedHashMap<>();
+        this.aggregateFieldToFieldCountAndSum = new HashMap<>();
     }
 
     /**
      * Merge a new tuple into the aggregate, grouping as indicated in the
      * constructor
-     * 
+     *
      * @param tup
      *            the Tuple containing an aggregate field and a group-by field
      */
     public void mergeTupleIntoGroup(Tuple tup) {
         // some code goes here
         // MIN, MAX, SUM, AVG, COUNT,
+        Field groupField = tup.getField(gbfield);
+        Tuple tuple = aggregateFieldToTuple.computeIfAbsent(groupField, key -> {
+            TupleDesc tupleDesc = new TupleDesc(
+                    new Type[]{gbfieldtype, Type.INT_TYPE},
+                    new String[]{tup.getTupleDesc().getFieldName(gbfield), operator.name()}
+            );
+            return new Tuple(tupleDesc);
+        });
+        tuple.setField(0, tup.getField(gbfield));
         if (operator.equals(Op.MIN)) {
-
-        } else if(operator.equals(Op.MAX)) {
-
-        } else if(operator.equals(Op.SUM)) {
-
-        } else if(operator.equals(Op.AVG)) {
-            tup.getField(gbfield);
-            for(Iterator<Field> fieldIterator = tup.fields(); fieldIterator.hasNext(); ) {
-                Field field = fieldIterator.next();
+            int fieldValue = ((IntField)tup.getField(afield)).getValue();
+            IntField field = (IntField)tuple.getField(1);
+            if(field != null) {
+                fieldValue = Math.min(fieldValue, field.getValue());
             }
-            tup.setField(gbfield, );
-        } else if(operator.equals(Op.COUNT)) {
-            tup.setField(gbfield, tuples[cursor]);
-            tuples[cursor] = tup;
+            tuple.setField(1, new IntField(fieldValue));
+        } else if (operator.equals(Op.MAX)) {
+            int fieldValue = ((IntField)tup.getField(afield)).getValue();
+            IntField field = (IntField)tuple.getField(1);
+            if(field != null) {
+                fieldValue = Math.max(fieldValue, field.getValue());
+            }
+            tuple.setField(1, new IntField(fieldValue));
+        } else if (operator.equals(Op.SUM)) {
+            IntField field = (IntField)tuple.getField(1);
+            int value = ((IntField)tup.getField(afield)).getValue();
+            if(field != null) {
+                value += field.getValue();
+            }
+            tuple.setField(1, new IntField(value));
+        } else if (operator.equals(Op.AVG)) {
+            int[] countAndSum = aggregateFieldToFieldCountAndSum.computeIfAbsent(groupField,
+                    k -> new int[]{0, 0}
+            );
+            ++countAndSum[0];
+            countAndSum[1] += ((IntField)tup.getField(afield)).getValue();
+            tuple.setField(1, new IntField(countAndSum[1] / countAndSum[0]));
+        } else if (operator.equals(Op.COUNT)) {
+            IntField field = (IntField)tuple.getField(1);
+            if(field == null) {
+                tuple.setField(1, new IntField(1));
+            } else {
+                tuple.setField(1, new IntField(field.getValue() + 1));
+            }
         }
     }
 
     /**
      * Create a OpIterator over group aggregate results.
-     * 
+     *
      * @return a OpIterator whose tuples are the pair (groupVal, aggregateVal)
      *         if using group, or a single (aggregateVal) if no grouping. The
      *         aggregateVal is determined by the type of aggregate specified in
@@ -94,38 +129,42 @@ public class IntegerAggregator implements Aggregator {
         return new OpIterator() {
             private int cursor;
 
-            private int len;
+            private boolean open;
+
+            private Iterator<Tuple> tuples;
 
             @Override
             public void open() throws DbException, TransactionAbortedException {
-
+                open = true;
+                tuples = aggregateFieldToTuple.values().iterator();
             }
 
             @Override
             public boolean hasNext() throws DbException, TransactionAbortedException {
-                return false;
+                return open && tuples.hasNext();
             }
 
             @Override
             public Tuple next() throws DbException, TransactionAbortedException, NoSuchElementException {
-                return null;
+                return open ? tuples.next() : null;
             }
 
             @Override
             public void rewind() throws DbException, TransactionAbortedException {
-
+                close();
+                open();
             }
 
             @Override
             public TupleDesc getTupleDesc() {
-                return null;
+                return tuples.next().getTupleDesc();
             }
 
             @Override
             public void close() {
-
+                open = false;
             }
-        }
+        };
     }
 
 }
