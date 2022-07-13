1b6c66df45b7b5991e0596a9e965116e9effb443
ShengyuFang <1123914164@qq.com>
2022-07-14 00:22:17 +0800

    implement IntHistogram

--- src/java/simpledb/optimizer/IntHistogram.java
+++ src/java/simpledb/optimizer/IntHistogram.java
@@ -2,9 +2,23 @@ package simpledb.optimizer;
 
 import simpledb.execution.Predicate;
 
+import java.util.Arrays;
+
 /** A class to represent a fixed-width histogram over a single integer-based field.
  */
 public class IntHistogram {
+
+    private int buckets;
+
+    private int min;
+
+    private int max;
+
+    private int width;
+
+    private int[] bucketArray;
+
+    private int count;
 
     /**
      * Create a new IntHistogram.
@@ -24,6 +38,13 @@ public class IntHistogram {
      */
     public IntHistogram(int buckets, int min, int max) {
     	// some code goes here
+        this.buckets = buckets;
+        this.min = min;
+        this.max = max;
+        this.bucketArray = new int[buckets];
+        // max - min + 1个数填入buckets个桶
+        // Math.ceil((max - min + 1) / buckets) = (max - min + 1 + buckets -1)/buckets
+        this.width = Math.max((max - min + buckets) / buckets, 1);
     }
 
     /**
@@ -32,6 +53,10 @@ public class IntHistogram {
      */
     public void addValue(int v) {
     	// some code goes here
+//        int index = (v-min)/((max - min + 1)/buckets);
+        int index = getBucketIndex(v);
+        bucketArray[index]++;
+        ++count;
     }
 
     /**
@@ -45,8 +70,61 @@ public class IntHistogram {
      * @return Predicted selectivity of this particular operator and value
      */
     public double estimateSelectivity(Predicate.Op op, int v) {
-
     	// some code goes here
+        int index = getBucketIndex(v);
+        if(op.equals(Predicate.Op.EQUALS) || op.equals(Predicate.Op.LIKE)) {
+            return ((double) bucketArray[index] / count);
+        } else if (op.equals(Predicate.Op.GREATER_THAN)) {
+            if(v > max) {
+                return 0.0;
+            }
+            if(v <= min) {
+                return 1.0;
+            }
+            int sum = 0;
+            for(int i = index + 1; i < buckets; ++i) {
+                sum += bucketArray[i];
+            }
+            return (double) (sum + ((index + 1) * width + min - 1 - v)*bucketArray[index]) / count;
+        } else if (op.equals(Predicate.Op.GREATER_THAN_OR_EQ)) {
+            if(v > max) {
+                return 0.0;
+            }
+            if(v <= min) {
+                return 1.0;
+            }
+            int sum = 0;
+            for(int i = index + 1; i < buckets; ++i) {
+                sum += bucketArray[i];
+            }
+            return (double) (sum + ((index + 1) * width + min - v)*bucketArray[index]) / count;
+        } else if (op.equals(Predicate.Op.LESS_THAN)) {
+            if(v < min) {
+                return 0.0;
+            }
+            if(v >= max) {
+                return 1.0;
+            }
+            int sum = 0;
+            for(int i = 0; i < index; ++i) {
+                sum += bucketArray[i];
+            }
+            return (double)(sum + (v - (index * width + min))*bucketArray[index]) / count;
+        } else if (op.equals(Predicate.Op.LESS_THAN_OR_EQ)) {
+            if(v < min) {
+                return 0.0;
+            }
+            if(v >= max) {
+                return 1.0;
+            }
+            int sum = 0;
+            for(int i = 0; i < index; ++i) {
+                sum += bucketArray[i];
+            }
+            return (double)(sum + (v - (index * width + min) + 1)*bucketArray[index]) / count;
+        } else if (op.equals(Predicate.Op.NOT_EQUALS)) {
+            return ((double) (count - bucketArray[index])) / count;
+        }
         return -1.0;
     }
     
@@ -61,7 +139,7 @@ public class IntHistogram {
     public double avgSelectivity()
     {
         // some code goes here
-        return 1.0;
+        return (double) count / buckets;
     }
     
     /**
@@ -69,6 +147,10 @@ public class IntHistogram {
      */
     public String toString() {
         // some code goes here
-        return null;
+        return Arrays.toString(bucketArray);
+    }
+
+    private int getBucketIndex(int v) {
+        return (v - min) / width;
     }
 }
